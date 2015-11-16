01a71970e37684c8524584f24c705af977087bf5
Harrison Oliver Evans <HOEva@dyn-72-33-51-125.uwnet.wisc.edu>
2015-11-15 18:19:44 -0600

    Initial Commit

---
+++ homeWork2Tests/homeWork2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  homeWork2Tests.swift
+//  homeWork2Tests
+//
+//  Created by Harrison Oliver Evans on 11/15/15.
+//  Copyright (c) 2015 Harrison Oliver Evans. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class homeWork2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
